fbe5a24eada3eb4010e8398ace8df2fe89db48a9
EstebanRM <EstebanRM@REAL-MADRID>
2018-12-25 11:55:26 -0500

    creamos Consultas para ahi realizar las consultas con la base de datos

---
+++ CRMsl/src/BD/Consultas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BD;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author EstebanRM
+ */
+public class Consultas {
+    private PreparedStatement sentencia=null;
+    private ResultSet resultado=null;
+    private Conexion con;
+    
+    
+}
